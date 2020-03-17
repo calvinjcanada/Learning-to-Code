0d52767136536ef6df1b42ac66f68b86a5a22d2a
Calvin Canada <13888849+calvinjcanada@users.noreply.github.com>
2020-03-17 16:03:57 -0500

    Create Population-Growth.js

---
+++ Javascript/Code-Snippets/Population-Growth.js
@@ -0,0 +1,28 @@
+function nbYear(p0, percent, aug, p) {
+    // Calculate the number of years needed to reach population "p"
+    //p0 = starting population
+    //percent = % of population growth (entered as a integer [must convert to percent number])
+    //aug = number of people who steadily migrate to the new place [static growth unaffected by percentage]
+    //p = final population
+    
+    //console.log('Start ' + p0);
+    //console.log('Percentage ' + percent);
+    //console.log('Population Increment ' + aug);
+    //console.log('Final Population ' + p);
+    
+    let percentage = percent / 100;
+    let population = p0;
+    let year = 0;
+    
+    while(population < p)
+    {
+      //console.log("Year " + year);
+      population += population * percentage;
+      //console.log(population);
+      population += aug;
+      //console.log(population + " with aug");
+      year++;
+    }
+    
+    return year;
+}
