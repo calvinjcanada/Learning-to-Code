c7bfe8233818c873ef8a3c571a290852d8bfb5bb
Calvin Canada <13888849+calvinjcanada@users.noreply.github.com>
2020-03-17 10:26:56 -0500

    Update Array-Comparison.js

--- Javascript/Code-Snippets/Array-Comparison.js
+++ Javascript/Code-Snippets/Array-Comparison.js
@@ -2,21 +2,14 @@
 // It removes values from array A that are also present in array B and returns the newly editted array A
 
 function arrayDiff(a, b) {
-  for (let i = 0; i < b.length; i++) {
-  //Loop through array B
-    //console.log(b[i]);
-    if(a.includes(b[i],0))
+  for (let i = 0; i < a.length; i++) {
+    if(b.includes(a[i],0))
     {
-      //console.log("Yes");
-      let index = a.indexOf(b[i]);
-      //console.log(index);
-      a.splice(index, index + 1);
-      //console.log(a);
-    }
-    else
-    {
-      //console.log("No");
-      continue;
+      a.splice(i,1);
+      
+      //Reset the index to the 1st element because splicing the array updates the indices
+      //This causes some elements to be skipped over
+      i = -1;
     }
   }
   return a;
