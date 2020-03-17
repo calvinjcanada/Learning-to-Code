661d2690739cc800974d0790e9616d560fa65ec1
Calvin Canada <13888849+calvinjcanada@users.noreply.github.com>
2020-03-17 16:05:17 -0500

    Update Population-Growth.js

--- Javascript/Code-Snippets/Population-Growth.js
+++ Javascript/Code-Snippets/Population-Growth.js
@@ -25,4 +25,10 @@ function nbYear(p0, percent, aug, p) {
     }
     
     return year;
+    
+    //Perfect solution
+    //function nbYear(p0, percent, aug, p) {
+   //for(var y = 0; p0 < p; y++) p0 = p0 * (1 + percent / 100) + aug;
+   //return y;
+}
 }
