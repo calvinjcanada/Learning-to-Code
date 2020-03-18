cd027e6b93aa0a82f4d15636224495ec77287837
Calvin Canada <13888849+calvinjcanada@users.noreply.github.com>
2020-03-17 21:08:45 -0500

    Create Phone-Number-Format.js

---
+++ Javascript/Code-Snippets/Phone-Number-Format.js
@@ -0,0 +1,7 @@
+function createPhoneNumber(numbers){
+//Takes in a string of 10 integers and converts into phone number format
+  
+  return `(${numbers[0]}${numbers[1]}${numbers[2]}) ${numbers[3]}${numbers[4]}${numbers[5]}-${numbers[6]}${numbers[7]}${numbers[8]}${numbers[9]}`
+
+  
+}
