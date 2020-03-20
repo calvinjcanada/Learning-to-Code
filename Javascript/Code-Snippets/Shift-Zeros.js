a110984e97bc2af8a5394b13485d33b02193ee3d
Calvin Canada <13888849+calvinjcanada@users.noreply.github.com>
2020-03-19 19:11:09 -0500

    Update Shift-Zeros.js

--- Javascript/Code-Snippets/Shift-Zeros.js
+++ Javascript/Code-Snippets/Shift-Zeros.js
@@ -19,4 +19,7 @@ var moveZeros = function (arr) {
   });
   
   return nonZeros;
+  
+  //Best Solution
+  // return arr.filter(function(x) {return x !== 0}).concat(arr.filter(function(x) {return x === 0;}));
 }
